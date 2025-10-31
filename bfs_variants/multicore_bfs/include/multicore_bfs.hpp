a34dff76c12bb2493d556a02452cbc33c568ced9
abhijit-kumar.sahu@hpe.com <abhijit-kumar.sahu@hpe.com>
2025-11-01 01:41:20 +0530

    bug fix

--- bfs_variants/multicore_bfs/include/multicore_bfs.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MULTICORE_BFS_H
-#define MULTICORE_BFS_H
-
-#define ALPHA 15.0
-#define BETA 24.0
-
-void multicore_bfs(int num_verts, long totalEdges, int* out_array, long* out_degree_list, int root, int* parents, int* levels);
-
-#endif // MULTICORE_BFS_H
